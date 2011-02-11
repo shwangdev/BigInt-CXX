d744bc3d2b3483b8925ef26e8a55f9591e65fa5c
Your Name <you@example.com>
2011-02-11 17:51:08 +0600

    Header file updated

--- src/bigint.h
+++ src/bigint.h
@@ -5,11 +5,46 @@ Class BigInt {
 
 public:
 	BigInt(const char * );
+	BigInt(const BigInt & );
+	BigInt (unsigned int );
+	BigInt (int size, int type)
 	BigInt(int );
+	BigInt ();
 	~BigInt();
+	
+	BigInt operator = (const BigInt & );
+	
+	/* Arithemtics */
+	BigInt operator + (const BigInt );
+	BigInt operator * (const BigInt & );
+	BigInt operator - (const BigInt & );
+	BigInt operator / (const BigInt & )
+	BigInt operator % (const BigInt & ); 
+	
+	/* Comparation */
+	bool BigInt operator == (const BigInt & ) const;
+	bool BigInt operator < (const BigInt & ) const;
+	bool BigInt operator <= (const BigInt & ) const;
+	bool BigInt operator > (const BigInt & ) const;
+	bool BigInt operator >= (const BigInt & ) const;
+	bool BigInt operator != (const BigInt & ) const;
+	
 
 private:
-
+    BASE * GetMem (int size);
+    void FreeMem (void);
+    int Size (void) const;
+    int SizeZ (void) const;
+    void Normalize (void);
+    void PrintHex (const char * name = "");
+    void PrintDec (const char * name = "");
+    BASE add(BASE * , BASE * , BASE * , BASE * , BASE * );
+    void mul(BASE * , BASE * , BASE * , BASE * , BASE * );
+    int sub(BASE * , BASE * , BASE * , BASE * , BASE * );
+    BigInt divb(const BASE , BASE & );
+    BigInt div(BigInt u, BigInt v, BigInt & );
+    BigInt divrm(BASE *, BASE *,BASE v,BASE & );
+    
 
 };
 
